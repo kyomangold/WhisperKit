be74d295810987c770341e2f0f2747a052e2e023
kyomangold <91421645+kyomangold@users.noreply.github.com>
2024-12-08 20:16:05 +0100

    Adjust swift-argument-parser version range

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/huggingface/swift-transformers.git", "0.1.8"..."0.1.13"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
     ],
     targets: [
         .target(
